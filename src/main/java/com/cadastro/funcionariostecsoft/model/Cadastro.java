0fdb0e33c7ae2effdd883f6e364c5bead8a2793f
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-31 13:04:53 -0300

    Criado a classe model e utilizado lombok

---
+++ src/main/java/com/cadastro/funcionariostecsoft/model/Cadastro.java
@@ -0,0 +1,104 @@
+package com.cadastro.funcionariostecsoft.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Setter
+@Getter
+@Entity
+@Table(name = "funcionarios")
+public class Cadastro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo; 
+	
+	@Column(name = "nome")
+	private String nome;
+	
+	@Column(name = "idade")
+	private int idade;
+	
+	@Column(name = "cargo")
+	private String cargo;
+	
+	
+	@Column(name = "endereco")
+	private String endereco;
+	
+	@Column(name ="telefone")
+	private String telefone;
+	
+	@Column(name = "email")
+	private String email;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cargo == null) ? 0 : cargo.hashCode());
+		result = prime * result + (int) (codigo ^ (codigo >>> 32));
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((telefone == null) ? 0 : telefone.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cadastro other = (Cadastro) obj;
+		if (cargo == null) {
+			if (other.cargo != null)
+				return false;
+		} else if (!cargo.equals(other.cargo))
+			return false;
+		if (codigo != other.codigo)
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (telefone == null) {
+			if (other.telefone != null)
+				return false;
+		} else if (!telefone.equals(other.telefone))
+			return false;
+		return true;
+	}
+	
+	
+
+	
+
+	
+
+}
