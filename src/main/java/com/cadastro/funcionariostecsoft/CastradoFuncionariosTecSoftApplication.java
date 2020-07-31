ee5a6e7b7d9e23e12253651e7ef0ab5cfad0b1fc
Gilmar Cruz Silva <supervingue67@gmail.com>
2020-07-31 11:23:53 -0300

    Criado a API cadastro de funcionarios empresa TecSoft

---
+++ src/main/java/com/cadastro/funcionariostecsoft/CastradoFuncionariosTecSoftApplication.java
@@ -0,0 +1,13 @@
+package com.cadastro.funcionariostecsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastradoFuncionariosTecSoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastradoFuncionariosTecSoftApplication.class, args);
+	}
+
+}
